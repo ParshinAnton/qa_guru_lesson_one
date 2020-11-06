26c89beafb4bc47dc2b9ec652eb9315ebbdcba29
Anton Parshin <anton.parshin@hemmersbach.com>
2020-11-06 16:35:23 +0100

    add some new files for continue

---
+++ src/test/java/tests/GoogleTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+
+
+
+class GoogleTest {
+    void selenideSearchTest() {
+
+
+
+    }
+}
+// Открыть google
+
+// Ввести Selenide в поиск
+
+// Проверить, что Selenide появился в результатах поиска
